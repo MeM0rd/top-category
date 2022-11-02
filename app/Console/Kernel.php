68965c30bb92a95a49d6674e924f68ff7fab2360
MeM0rd <95081246+MeM0rd@users.noreply.github.com>
2022-11-02 18:10:44 +0300

    фикс

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('request-categories')->everyMinute();
+         $schedule->command('request-categories')->everyThirtyMinutes();
     }
 
     /**
